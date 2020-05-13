e9de986b00dd63eaefe9658aff848de938a2ecc3
t190007 <t190007@mail.ryukoku.ac.jp>
2020-05-13 08:17:54 +0000

    提出

--- kadai5-1.c
+++ kadai5-1.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-int main(){
-    
+int main(){  
     double a=1.0,b;
     while(a>0.0){
         a=a/2;
-        if(a>0.0)
-            b=a;
+        if(a>0.0)b=a;
     }
     printf("%e\n",b);
 }
